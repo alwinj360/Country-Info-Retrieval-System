636fa359c634ab8be51ea14ede6e2490cead3881
Alwin J. Kowattu <138890707+alwinj360@users.noreply.github.com>
2024-07-24 14:49:33 -0700

    Create mainBSTDict.py

---
+++ mainBSTDict.py
@@ -0,0 +1,9 @@
+# Written by: Alwin
+# Description: This file calls the interface, the rest of the program takes place through the command line. Hence, the testing has been done in the command line.
+from dict.bstdict import *
+from irsystems.countryIRSystemBSTDict import *
+from otherFiles.interface import *
+
+  
+if __name__ == "__main__":
+  Interface.country_irsystem_Cl_dict()
