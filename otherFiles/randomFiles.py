12ce94dc67adc43daec2fef3ad5c1112e93213bc
Alwin J. Kowattu <138890707+alwinj360@users.noreply.github.com>
2025-10-12 13:42:13 -0700

    Update randomFiles.py
    1. Removed unused code (generate_data method).
    2. Added a header row to generate_data_csv method because all IRSystem files skip the first row (original country data had a header).
    3. Excluded header row from randomization.
    4. Added a "newline=''" parameter to the final writefile code block to prevent blank lines between entries.

--- otherFiles/randomFiles.py
+++ otherFiles/randomFiles.py
@@ -2,46 +2,19 @@ import random
 import os
 import csv
 
-
-def generate_data(rand_file_name, no_entries):
-  # rand_file_name: the name of the random 
-  # no_entries: the number of entries in the file
-  
-  file_name = "temp.txt" # temp file to hold the generated entries
-  with open(file_name, 'w') as writefile:
-    for i in range(1, no_entries+1):
-      time = 2020
-      time_code = "YR2020"
-      country_name = "India" + str(i)
-      country_code = "IND"
-      population_total = 1400000000
-      life_expectancy = 70
-      access_to_clean_fuels = 90
-      using_basic_drinking_water = 90
-      GDP_per_capita = 15000
-      writefile.write(str(time) + " " + time_code + " " + country_name + " " + country_code + " " + str(population_total) + " " + str(life_expectancy) + " " + str(access_to_clean_fuels) + " " + str(using_basic_drinking_water) + " " + str(GDP_per_capita) + "\n")
-
-
-  # randmize the entries in the file
-  with open(file_name,'r') as source:
-    data = [ (random.random(), line) for line in source ]
-  data.sort()
-  with open(rand_file_name,'w') as target:
-    for _, line in data:
-        target.write( line )
-
-  # removing the temp file, not needed anymore
-  os.remove(file_name)
-
-
 # Generating random data. The car information is not important, rather the car vin number needs to be unique
 def generate_data_csv(rand_file_name, no_entries):
   # rand_file_name: the name of the random 
   # no_entries: the number of entries in the file
   
   file_name = "temp.csv" # temp file to hold the generated entries
-  with open(file_name, 'w', encoding='UTF8') as f:
+  with open(file_name, 'w', encoding='UTF8', newline='') as f:
     writefile = csv.writer(f)
+    header = ["Time", "Time Code", "Country Name", "Country Code", 
+                  "Population, total", "Life expectancy at birth", 
+                  "Access to clean fuels", "Basic drinking water", 
+                  "GDP per capita"]
+    writefile.writerow(header)
     for i in range(1, no_entries+1):
       time = 2020
       time_code = "YR2020"
@@ -58,9 +31,10 @@ def generate_data_csv(rand_file_name, no_entries):
   # randmize the entries in the file
   with open(file_name,'r') as source:
     csvreader = csv.reader(source)
+    header = next(csvreader) # Read and save header (skip it)
     data = [ (random.random(), line) for line in csvreader ]
   data.sort()
-  with open(rand_file_name,'w') as target:
+  with open(rand_file_name,'w', encoding='UTF8', newline='') as target:
     writefile = csv.writer(target)
     for _, line in data:
         writefile.writerow( line )
